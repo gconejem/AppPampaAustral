8e1401129d76bdcb92e43df9ea4bc723995cb3ac
nicolasbragaobnki <nicolas.bragaobnki@gmail.com>
2025-05-26 21:36:20 -0400

    CSV en obras considera tildes

--- src/views/apps/works/list/WorkListTable.tsx
+++ src/views/apps/works/list/WorkListTable.tsx
@@ -378,8 +378,12 @@ const WorkListTable = () => {
       // Convertir a formato CSV
       const csvContent = [headers.join(','), ...csvData.map(row => row.map(cell => `"${cell}"`).join(','))].join('\n')
 
+      // Agregar BOM UTF-8
+      const BOM = '\uFEFF'
+      const csvContentWithBOM = BOM + csvContent
+
       // Crear el blob y descargar
-      const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' })
+      const blob = new Blob([csvContentWithBOM], { type: 'text/csv;charset=utf-8;' })
       const link = document.createElement('a')
       const url = URL.createObjectURL(blob)
 
