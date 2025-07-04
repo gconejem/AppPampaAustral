4c17b788f1648e167278a1dfa417123bb0a90444
nicolasbragaobnki <nicolas.bragaobnki@gmail.com>
2025-07-04 13:48:25 -0400

    Se corrige funcionamiento boton Cerrar en crear cotizacion

--- src/views/apps/invoice/add/AddHeader.tsx
+++ src/views/apps/invoice/add/AddHeader.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import { useRouter } from 'next/navigation'
 import { Card, CardContent, Typography, Button } from '@mui/material'
 
 const AddHeader: React.FC = () => {
+  const router = useRouter()
   return (
     <Card elevation={0} style={{ marginBottom: '16px' }}>
       <CardContent style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '16px' }}>
         <Typography variant='h6' color='textPrimary'>
           Nueva Cotización
         </Typography>
-        <Button variant='outlined' sx={{ color: '#454343', borderColor: '#474b4f' }}>
+        <Button variant='outlined' sx={{ color: '#454343', borderColor: '#474b4f' }} onClick={() => router.back()}>
           Cerrar
         </Button>
       </CardContent>
