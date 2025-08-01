bfeab174884da4187cf0d438e4e7087595f04f92
nicolasbragaobnki <nicolas.bragaobnki@gmail.com>
2025-08-01 14:29:53 -0400

    Se agregan campos observaciones para cambios de estado en Agenda

---
+++ prisma/migrations/20250801182923_update_agenda_add_campos_observaciones_estados/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Agenda" ADD COLUMN     "observacionAnulada" TEXT,
+ADD COLUMN     "observacionEnRevision" TEXT,
+ADD COLUMN     "observacionRecibidaOK" TEXT,
+ADD COLUMN     "observacionSuspendida" TEXT;
