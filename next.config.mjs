d6396dee007a1b5df2ab188e6b54777627773c9e
nicolasbragaobnki <nicolas.bragaobnki@gmail.com>
2025-11-26 00:11:49 -0300

    Agregar configuración de minificación y eliminación de consola en next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    removeConsole: true,
+  },
   typescript: {
     ignoreBuildErrors: true, // ignora errores de TypeScript
   },
