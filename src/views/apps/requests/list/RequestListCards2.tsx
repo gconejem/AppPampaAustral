0ebf517bb816fbc07d1b41e817709e132079bac0
Guido C <guidoconejeros@gmail.com>
2025-12-02 13:53:35 -0300

    roles y permisos solicitudes

--- src/views/apps/requests/list/RequestListCards2.tsx
+++ src/views/apps/requests/list/RequestListCards2.tsx
@@ -8,8 +8,23 @@ import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 
+// Imports para permisos - NUEVO
+import { usePermissions } from '@/hooks/usePermissions'
+import { permisos } from '@/permisos/permisos'
+
+
 function RequestListCards2() {
   const router = useRouter()
+
+    // Hook de permisos
+  const { hasPermission } = usePermissions()
+  
+  // Verificar si el usuario solo tiene permisos de lectura
+  const soloLectura =
+    hasPermission(permisos.empresa.ver) &&
+    !hasPermission(permisos.empresa.crear) &&
+    !hasPermission(permisos.empresa.editar) &&
+    !hasPermission(permisos.empresa.eliminar)
 
   return (
     <Grid container spacing={2} alignItems='center' justifyContent='space-between' style={{ marginBottom: '24px' }}>
@@ -22,6 +37,7 @@ function RequestListCards2() {
               </Grid>
               <Grid item>
                 <Button
+                disabled={soloLectura}
                   variant='contained'
                   color='primary'
                   startIcon={<i className='ri-add-line' />}
