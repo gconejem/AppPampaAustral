608f7cc985d112575b2657c95e42ab8532400f0c
nico <nicolas.bragaobnki@gmail.com>
2026-04-13 00:12:56 -0400

    fix: al desmarcar fecha de vencimiento las submuestras se eliminan

--- src/views/apps/internalcontrol/components/RcmDraftForm.tsx
+++ src/views/apps/internalcontrol/components/RcmDraftForm.tsx
@@ -429,7 +429,7 @@ const RcmDraftForm: React.FC<RcmDraftFormProps> = ({
                                             onChange={(e) => { setErrorVencimiento(''); setCantidadMuestras(e.target.value) }} required fullWidth />
                                     </Grid>
                                     <Grid item xs={12} md={3}>
-                                        <FormControlLabel control={<Checkbox checked={tieneVencimiento} onChange={(e) => setTieneVencimiento(e.target.checked)} />} label='Vencimiento (activa Submuestras)' />
+                                        <FormControlLabel control={<Checkbox checked={tieneVencimiento} onChange={(e) => { setTieneVencimiento(e.target.checked); if (!e.target.checked) setSubmuestrasVencimiento([]) }} />} label='Vencimiento (activa Submuestras)' />
                                     </Grid>
                                     <Grid item xs={12} md={3}>
                                         <FormControlLabel control={<Checkbox checked={informeEnsayo} onChange={(e) => setInformeEnsayo(e.target.checked)} />} label='Informe Ensayo' />
