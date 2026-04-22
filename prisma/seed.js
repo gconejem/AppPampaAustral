bf8bb0f8271e11bc2fe9090d7f9afb7008c6dff3
nico <nicolas.bragaobnki@gmail.com>
2026-04-21 22:48:38 -0400

    fix: se quitan saltos de linea innecesarios

--- prisma/seed.js
+++ prisma/seed.js
@@ -7,17 +7,11 @@ async function main() {
 
   const areas = [
     { nombre: 'Suelo' },
-
     { nombre: 'Hormigón' },
-
     { nombre: 'Asfalto' },
-
     { nombre: 'Elementos y Componentes' },
-
     { nombre: 'Otros' },
-
     { nombre: 'Servicios' },
-
     { nombre: 'Áridos' }
   ]
 
@@ -28,7 +22,6 @@ async function main() {
       await prisma.area.create({
         data: area
       })
-
       console.log(`Área "${area.nombre}" creada exitosamente`)
     } catch (error) {
       if (error.code === 'P2002') {
@@ -47,61 +40,33 @@ async function main() {
 
   const familias = [
     // Suelo
-
     { nombre: 'Controles Suelo', areaId: areasCreadas.find(a => a.nombre === 'Suelo')?.id },
-
     { nombre: 'Análisis Suelo', areaId: areasCreadas.find(a => a.nombre === 'Suelo')?.id },
-
     { nombre: 'Mecánica de Suelo', areaId: areasCreadas.find(a => a.nombre === 'Suelo')?.id },
-
     { nombre: 'EMS - Ing', areaId: areasCreadas.find(a => a.nombre === 'Suelo')?.id },
-
     { nombre: 'Otros', areaId: areasCreadas.find(a => a.nombre === 'Suelo')?.id },
-
     // Hormigón
-
     { nombre: 'Hormigón Fresco', areaId: areasCreadas.find(a => a.nombre === 'Hormigón')?.id },
-
     { nombre: 'Hormigón Edurecido', areaId: areasCreadas.find(a => a.nombre === 'Hormigón')?.id },
-
     { nombre: 'Dosificaciones Hormigón', areaId: areasCreadas.find(a => a.nombre === 'Hormigón')?.id },
-
     { nombre: 'Áridos para Hormigón / Mortero', areaId: areasCreadas.find(a => a.nombre === 'Hormigón')?.id },
-
     { nombre: 'Áridos', areaId: areasCreadas.find(a => a.nombre === 'Hormigón')?.id },
-
     { nombre: 'Otros', areaId: areasCreadas.find(a => a.nombre === 'Hormigón')?.id },
-
     // Asfalto
-
     { nombre: 'Control Terreno Asfalto', areaId: areasCreadas.find(a => a.nombre === 'Asfalto')?.id },
-
     { nombre: 'Testigos y Mezclas Asfálticas', areaId: areasCreadas.find(a => a.nombre === 'Asfalto')?.id },
-
     { nombre: 'Dosificaciones Asfalto', areaId: areasCreadas.find(a => a.nombre === 'Asfalto')?.id },
-
     { nombre: 'Áridos para Asfalto', areaId: areasCreadas.find(a => a.nombre === 'Asfalto')?.id },
-
     { nombre: 'Otros', areaId: areasCreadas.find(a => a.nombre === 'Asfalto')?.id },
-
     // Elementos y Componentes
-
     { nombre: 'Elementos y Componentes', areaId: areasCreadas.find(a => a.nombre === 'Elementos y Componentes')?.id },
-
     // Áridos
-
     { nombre: 'Análisis de Áridos', areaId: areasCreadas.find(a => a.nombre === 'Áridos')?.id },
-
     { nombre: 'Otros', areaId: areasCreadas.find(a => a.nombre === 'Áridos')?.id },
-
     // Otros
-
     { nombre: 'Pintura', areaId: areasCreadas.find(a => a.nombre === 'Otros')?.id },
-
     { nombre: 'Otros', areaId: areasCreadas.find(a => a.nombre === 'Otros')?.id },
-
     // Servicios
-
     { nombre: 'Servicios', areaId: areasCreadas.find(a => a.nombre === 'Servicios')?.id }
   ]
 
@@ -113,11 +78,9 @@ async function main() {
         await prisma.familia.create({
           data: {
             nombre: familia.nombre,
-
             areaId: familia.areaId
           }
         })
-
         console.log(`Familia "${familia.nombre}" creada exitosamente`)
       } catch (error) {
         if (error.code === 'P2002') {
@@ -135,10 +98,8 @@ async function main() {
 main()
   .catch(e => {
     console.error(e)
-
     process.exit(1)
   })
-
   .finally(async () => {
     await prisma.$disconnect()
   })
