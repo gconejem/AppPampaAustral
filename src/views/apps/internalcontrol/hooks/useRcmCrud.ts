139d179f510a03d789bd83f4ff4fdff04a711829
nico <nicolas.bragaobnki@gmail.com>
2026-06-16 22:43:22 -0400

    fix: add fechaConfeccion to form values in useRcmCrud

--- src/views/apps/internalcontrol/hooks/useRcmCrud.ts
+++ src/views/apps/internalcontrol/hooks/useRcmCrud.ts
@@ -455,6 +455,7 @@ export function useRcmCrud({
                     fechaServicio: rcm.fechaServicio,
                     fechaMuestreo: rcm.fechaMuestreo || rcm.fechaServicio,
                     fechaIngreso: today,
+                    fechaConfeccion: rcm.fechaConfeccion,
                     numeroTarjeta: rcm.numeroTarjeta,
                     tipoMaterial: rcm.tipoMaterial,
                     item: rcm.item,
